4df9a6777fb3a14c00476d49c90ed358ec9d96de
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-05-20 23:38:38 -0300

    ✨ feat: add jwt and cors plugins

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,22 @@
+import "dotenv/config";
 import fastify from "fastify";
+import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
+
 import { userRoutes } from "./routes/user";
+import { authRoutes } from "./routes/auth";
 
 const app = fastify();
 
-app.register(userRoutes);
+app.register(cors, {
+  origin: "*",
+});
+app.register(jwt, {
+  secret: process.env.JWT_SECRET as string,
+});
 
-console.log("Server is starting...");
+app.register(userRoutes);
+app.register(authRoutes);
 
 app
   .listen({
