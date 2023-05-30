23fdde530adf4e5aaef4d91f3618d07a7ff935b4
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-05-30 17:29:38 -0300

    ✨ feat: moved user schemas

--- packages/server/src/routes/auth.ts
+++ packages/server/src/routes/auth.ts
@@ -1,16 +1,11 @@
 import type { FastifyInstance } from "fastify";
-import { z } from "zod";
 import { prisma } from "../lib/prisma";
 import bcrypt from "bcrypt";
+import { userLoginSchema, userRegisterSchema } from "../schemas/user";
 
 export async function authRoutes(app: FastifyInstance) {
   app.post("/auth/login", async (request, reply) => {
-    const bodySchema = z.object({
-      email: z.string().email({}),
-      password: z.string(),
-    });
-
-    const { email, password } = bodySchema.parse(request.body);
+    const { email, password } = userLoginSchema.parse(request.body);
 
     const user = await prisma.user.findUniqueOrThrow({
       where: {
@@ -42,30 +37,9 @@ export async function authRoutes(app: FastifyInstance) {
   });
 
   app.post("/auth/register", async (request, reply) => {
-    const userSchema = z.object({
-      name: z.string({
-        required_error: "Name is required",
-      }),
-      username: z.string({
-        required_error: "Username is required",
-      }),
-      email: z
-        .string({
-          required_error: "Email is required",
-        })
-        .email({
-          message: "Invalid email",
-        }),
-      password: z
-        .string({
-          required_error: "Password is required",
-        })
-        .min(8, {
-          message: "Password must have at least 8 characters",
-        }),
-    });
-
-    const { email, name, password, username } = userSchema.parse(request.body);
+    const { email, name, password, username } = userRegisterSchema.parse(
+      request.body
+    );
 
     const usernameAlreadyExists = await prisma.user.findUnique({
       where: { username },
---
+++ packages/server/src/schemas/user.ts
@@ -0,0 +1,37 @@
+import { z } from "zod";
+
+export const userLoginSchema = z.object({
+  email: z
+    .string({
+      required_error: "Email is required",
+    })
+    .email({
+      message: "Invalid email",
+    }),
+  password: z.string({
+    required_error: "Password is required",
+  }),
+});
+
+export const userRegisterSchema = z.object({
+  name: z.string({
+    required_error: "Name is required",
+  }),
+  username: z.string({
+    required_error: "Username is required",
+  }),
+  email: z
+    .string({
+      required_error: "Email is required",
+    })
+    .email({
+      message: "Invalid email",
+    }),
+  password: z
+    .string({
+      required_error: "Password is required",
+    })
+    .min(8, {
+      message: "Password must have at least 8 characters",
+    }),
+});
