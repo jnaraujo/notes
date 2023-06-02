3afefb2cdd6a87ab76757c0bd640304eb44329e9
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-06-02 18:21:45 -0300

    ✨ feat: improved dashboard

--- packages/web/src/app/(logged)/dashboard/layout.tsx
+++ packages/web/src/app/(logged)/dashboard/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <aside className="group w-16 hover:w-48 bg-zinc-800 flex-shrink-0 shadow-lg z-10 transition-all duration-100">
         <DashNavbar />
       </aside>
-      <section className="flex-1 p-4">
+      <section className="flex-1 p-4 md:p-8">
         {children}
       </section>
     </main >
--- packages/web/src/app/(logged)/dashboard/page.tsx
+++ packages/web/src/app/(logged)/dashboard/page.tsx
@@ -1,18 +1,25 @@
-"use client"
-
-import { useUserStore } from "@/stores/user";
-import { useEffect } from "react";
+import Note from "@/components/Note";
 
 export default async function Dashboard() {
-  const { user } = useUserStore();
-
-  useEffect(() => {
-    console.log(user)
-  }, [user])
 
   return (
-    <h2 className="text-lg">
-      Bem-vindo {user?.name}!
-    </h2>
+    <>
+      <div className="space-y-1">
+        <h1 className="text-lg font-extrabold text-zinc-500">
+          Bem-vindo, Usuário!
+        </h1>
+      </div>
+
+      <h2 className="text-xl font-bold text-zinc-600 mt-6">
+        Suas últimas notas:
+      </h2>
+      <div className="flex flex-col gap-2 mt-2">
+        <Note title="Como eu criei um site que gera sites?" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        voluptatum, quibusdam, quia, quos voluptates voluptate quod
+        voluptatibus quas doloribus quidem voluptatem. Quisquam voluptatum,
+        quibusdam, quia, quos voluptates voluptate quod voluptatibus quas
+        doloribus quidem voluptatem." views={900000} url="#" />
+      </div>
+    </>
   )
 }
