da0dde61782e11e36222b8d275595c80ebc4cacd
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-06-02 14:13:10 -0300

    ✨ feat: add auth lib

--- packages/server/src/routes/auth.ts
+++ packages/server/src/routes/auth.ts
@@ -1,22 +1,33 @@
 import type { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
 import bcrypt from "bcrypt";
-import { userLoginSchema, userRegisterSchema } from "../schemas/user";
+import {
+  userLoginSchema,
+  userRegisterSchema,
+  usernameSchema,
+} from "../schemas/user";
 
 export async function authRoutes(app: FastifyInstance) {
-  app.post("/auth/login", async (request, reply) => {
+  app.post("/auth/signin", async (request, reply) => {
     const { email, password } = userLoginSchema.parse(request.body);
 
-    const user = await prisma.user.findUniqueOrThrow({
+    const user = await prisma.user.findUnique({
       where: {
         email,
       },
     });
 
+    if (!user) {
+      reply.status(401).send({
+        error: "User not found",
+      });
+      return;
+    }
+
     const doesPasswordMatch = bcrypt.compareSync(password, user.password);
 
     if (!doesPasswordMatch) {
-      reply.status(400).send({
+      reply.status(401).send({
         error: "Invalid password",
       });
       return;
@@ -36,7 +47,7 @@ export async function authRoutes(app: FastifyInstance) {
     reply.status(200).send(token);
   });
 
-  app.post("/auth/register", async (request, reply) => {
+  app.post("/auth/signup", async (request, reply) => {
     const { email, name, password, username } = userRegisterSchema.parse(
       request.body
     );
@@ -85,5 +96,29 @@ export async function authRoutes(app: FastifyInstance) {
     );
 
     reply.status(201).send(token);
+  });
+
+  app.get("/auth/username/:username", async (request, reply) => {
+    const { username } = usernameSchema.parse(request.params);
+
+    const doesUsernameExist = await prisma.user.findUnique({
+      where: {
+        username,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    if (!doesUsernameExist) {
+      reply.status(404).send({
+        error: "Username not found",
+      });
+      return;
+    }
+
+    reply.status(200).send({
+      exists: true,
+    });
   });
 }
