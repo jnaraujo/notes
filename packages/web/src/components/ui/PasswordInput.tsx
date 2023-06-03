d2956397221975817db11184a45704321600df3a
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-06-03 10:43:45 -0300

    ✨ feat:(PasswordInput) only show ShowPassword if input has any text

--- packages/web/src/components/ui/PasswordInput.tsx
+++ packages/web/src/components/ui/PasswordInput.tsx
@@ -6,9 +6,14 @@ import { Eye, EyeOff } from "lucide-react";
 import ErrorLabel from "./ErrorLabel";
 import clsx from "clsx";
 
-export default function PasswordInput({ error, className, ...props }: InputProps) {
+export default function PasswordInput({
+  error,
+  className,
+  ...props
+}: InputProps) {
   const passwordInputRef = useRef<HTMLInputElement>(null);
   const [showPassword, setShowPassword] = useState(false);
+  const [isInputEmpty, setIsInputEmpty] = useState(true);
 
   function handleShowPassword() {
     setShowPassword(!showPassword);
@@ -18,16 +23,19 @@ export default function PasswordInput({ error, className, ...props }: InputProps
     passwordInputRef.current?.focus();
   }
 
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setIsInputEmpty(event.target.value === "");
+  }
+
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between gap-1 overflow-hidden rounded-md border border-gray-400 bg-white outline-1 outline-black focus-within:outline">
         <Input
-          className={
-            clsx(
-              "border-none bg-transparent p-2 focus:outline-none",
-              className
-            )
-          }
+          className={clsx(
+            "border-none bg-transparent p-2 focus:outline-none",
+            className
+          )}
+          onChange={handleInputChange}
           type={showPassword ? "text" : "password"}
           placeholder="***********"
           onFocus={handlePasswordInputFocus}
@@ -35,7 +43,9 @@ export default function PasswordInput({ error, className, ...props }: InputProps
         />
         <button
           type="button"
-          className="mr-2"
+          className={clsx("mr-2", {
+            hidden: isInputEmpty,
+          })}
           tabIndex={-1}
           onClick={handleShowPassword}
         >
