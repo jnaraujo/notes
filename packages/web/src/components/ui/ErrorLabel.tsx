087a8043840a9cc0c710c0108066c684fdde35aa
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-06-03 11:28:52 -0300

    🐛 fix: ErrorLabel missing props

--- packages/web/src/components/ui/ErrorLabel.tsx
+++ packages/web/src/components/ui/ErrorLabel.tsx
@@ -1,7 +1,7 @@
 import clsx from "clsx";
 import { AlertCircle } from "lucide-react";
 
-interface ErrorLabel extends React.InputHTMLAttributes<HTMLInputElement> {
+interface ErrorLabel extends React.InputHTMLAttributes<HTMLSpanElement> {
   error: string;
 }
 
@@ -9,6 +9,7 @@ export default function ErrorLabel({ error, className, ...props }: ErrorLabel) {
   return (
     <span
       className={clsx("flex flex-1 items-center gap-1 text-red-500", className)}
+      {...props}
     >
       <AlertCircle size={16} /> {error}
     </span>
