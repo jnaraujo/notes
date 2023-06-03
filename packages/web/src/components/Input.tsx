0ac1f653491be9cc3cc12a13794053dd203daebb
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-06-02 21:32:28 -0300

    ✨ feat: improved Input css

--- packages/web/src/components/Input.tsx
+++ packages/web/src/components/Input.tsx
@@ -1,10 +1,10 @@
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> { }
 
 export default function ({ className, ...props }: InputProps) {
   return (
     <input
       className={
-        "flex-1 rounded-md border border-gray-400 p-2 outline-1 outline-black " +
+        "w-full rounded-md border border-gray-400 p-2 outline-1 outline-black " +
         className
       }
       {...props}
