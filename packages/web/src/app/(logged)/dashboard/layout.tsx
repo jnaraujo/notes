36d26534ac3fcbbc9aaf8dc89e5047a0872e917a
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-06-04 23:40:11 -0300

    ✨ feat: improve dashboard layout

--- packages/web/src/app/(logged)/dashboard/layout.tsx
+++ packages/web/src/app/(logged)/dashboard/layout.tsx
@@ -6,7 +6,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <aside className="group z-10 w-16 flex-shrink-0 bg-zinc-800 shadow-lg transition-all duration-100 hover:w-48">
         <DashNavbar />
       </aside>
-      <section className="flex-1 p-4 md:p-8">{children}</section>
+      <section className="mx-auto max-w-screen-xl flex-1 p-4 md:p-8">
+        {children}
+      </section>
     </main>
   );
 }
