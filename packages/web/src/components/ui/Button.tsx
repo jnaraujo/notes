288f17be9897ffab5bae3ce64314f6d4290c9545
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-06-08 15:02:34 -0300

    ✨ feat: improved Button

--- packages/web/src/components/ui/Button.tsx
+++ packages/web/src/components/ui/Button.tsx
@@ -12,7 +12,7 @@ export default function Button({ children, className, isLoading, loadingText, ..
     <button
       disabled={isLoading}
       className={clsx(
-        "w-full rounded-md bg-transparent border border-zinc-500 p-2 text-white transition-all duration-300 focus:border-zinc-400 focus:outline-none hover:border-zinc-400 hover:bg-gradient-to-tr from-zinc-900 to-zinc-950",
+        "w-full rounded-md bg-zinc-50 p-2 text-zinc-900 font-bold hover:bg-gradient-to-tr from-zinc-300 to-zinc-50",
         {
           "opacity-50 cursor-not-allowed": isLoading,
         },
