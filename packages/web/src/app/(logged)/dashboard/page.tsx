a9afb3b024496373e16225d2faee0d6b4b7822a1
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-06-08 18:21:09 -0300

    ✨ feat: load data on client

--- packages/web/src/app/(logged)/dashboard/page.tsx
+++ packages/web/src/app/(logged)/dashboard/page.tsx
@@ -1,20 +1,7 @@
 import LatestNotes from "@/components/Widgets/LatestNotes";
-import getQueryClient from "@/lib/query/getQueryClient";
-import { dehydrate } from "@tanstack/react-query";
-import Hydrate from "@/lib/query/hydrate.client";
-import { cookies } from "next/headers";
-import { fetchNotes } from "@/lib/notes";
 
 export default async function Dashboard() {
-  const queryClient = getQueryClient();
-  await queryClient.prefetchQuery(["latest-notes"], () =>
-    fetchNotes(cookies().get("token")?.value as string, 5)
-  );
-  const dehydratedState = dehydrate(queryClient);
-
   return (
-    <Hydrate state={dehydratedState}>
-      <LatestNotes />
-    </Hydrate>
+    <LatestNotes />
   );
 }
