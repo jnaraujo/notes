5d26c9501d393b6feb1c2bb9b1f28085354485f8
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-06-09 17:09:47 -0300

    🐛 fix: navbar height

--- packages/web/src/app/(logged)/dashboard/layout.tsx
+++ packages/web/src/app/(logged)/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import DashboardNavbar from "@/components/DashboardNavbar";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="flex h-screen">
-      <aside className="group z-10 w-16 flex-shrink-0 border-r border-zinc-500 bg-zinc-900/20 transition-all duration-100 hover:w-48">
+      <aside className="fixed h-full group z-10 w-16 flex-shrink-0 border-r border-zinc-500 bg-zinc-900/20 transition-all duration-100 hover:w-48">
         <DashboardNavbar />
       </aside>
       <section className="mx-auto mt-12 max-w-screen-md flex-1 p-4 md:p-0">
