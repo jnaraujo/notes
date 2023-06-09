077bd83c65fb61d0cf3246d127ef064da2d24870
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-06-09 11:39:54 -0300

    🔥 setup prisma log

--- packages/server/src/lib/prisma.ts
+++ packages/server/src/lib/prisma.ts
@@ -1,3 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient({});
+const isDevelopment = process.env.NODE_ENV === "development";
+
+export const prisma = new PrismaClient({
+  errorFormat: isDevelopment ? "minimal" : "pretty",
+  log: [isDevelopment ? "query" : "warn"],
+});
