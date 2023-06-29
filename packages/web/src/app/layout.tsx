8520174ffc97af63e7e027c5077715da5b59779d
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-06-29 11:18:22 -0300

    🐛 fix: font do not loading

--- packages/web/src/app/layout.tsx
+++ packages/web/src/app/layout.tsx
@@ -2,11 +2,16 @@ import Provider from "@/components/Provider";
 import "./globals.css";
 import { Inter, Roboto_Slab } from "next/font/google";
 
-const sans = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const sans = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+});
 const serif = Roboto_Slab({
   subsets: ["latin"],
   weight: ["400", "700"],
   variable: "--font-roboto-slab",
+  display: "swap",
 });
 
 export const metadata = {
@@ -21,9 +26,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${sans.variable} ${serif.variable}`}>
-      <body
-        className={`bg-zinc-950 text-zinc-200`}
-      >
+      <body className={`bg-zinc-950 text-zinc-200`}>
         <Provider>{children}</Provider>
       </body>
     </html>
