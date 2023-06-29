33804c75ab6bfc84561b2d550f13d7599e2e9b0a
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-06-29 13:34:46 -0300

    ✨ feat: improved dashboard navbar

--- packages/web/src/app/(logged)/dashboard/layout.tsx
+++ packages/web/src/app/(logged)/dashboard/layout.tsx
@@ -1,12 +1,20 @@
+import { User } from "@/@types/user";
 import DashboardNavbar from "@/components/DashboardNavbar";
+import { fetchUser } from "@/lib/user";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const loggedUser = (await fetchUser()) as User;
+
   return (
     <main className="flex h-screen">
-      <aside className="fixed h-full group z-10 w-16 flex-shrink-0 border-r border-zinc-500 bg-zinc-900/20 transition-all duration-100 hover:w-48">
-        <DashboardNavbar />
+      <aside className="group z-10 h-full w-56 flex-shrink-0 bg-zinc-900/20 transition-all duration-100">
+        <DashboardNavbar user={loggedUser} />
       </aside>
-      <section className="mx-auto mt-12 max-w-screen-md flex-1 p-4 md:p-0">
+      <section className="mx-auto mt-12 max-w-screen-md flex-1 p-4">
         {children}
       </section>
     </main>
--- packages/web/src/components/DashboardNavbar.tsx
+++ packages/web/src/components/DashboardNavbar.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import { Home, Feather, LucideIcon } from "lucide-react";
+import { Home, LogOut, LucideIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
 import Link from "next/link";
-import { createNote } from "@/lib/notes";
-import Cookies from "js-cookie";
-import { useRouter } from "next/navigation";
+import { User } from "@/@types/user";
+import Tooltip from "./ui/Tooltip";
 
 const MENU_LINKS = [
   {
@@ -15,39 +14,42 @@ const MENU_LINKS = [
     url: "/dashboard",
     isOnPath: (path: string) => path === "/dashboard",
   },
-  {
-    name: "Criar nota",
-    icon: Feather,
-    isOnPath: (path: string) => false,
-    onClick: (Router: any) => {
-      createNote(Cookies.get("token") as string, "Nota sem título", "").then(
-        (res) => {
-          Router.push(`/dashboard/draft/${res.id}`);
-        }
-      );
-    },
-  },
 ];
 
-export default function DashboardNavbar() {
+interface Props {
+  user: User;
+}
+
+export default function DashboardNavbar({ user }: Props) {
   const pathname = usePathname();
-  const Router = useRouter();
 
   return (
-    <nav className="flex h-full flex-col items-center justify-center gap-2 overflow-hidden">
-      {MENU_LINKS.map(({ name, url, icon: Icon, onClick, isOnPath }) => (
-        <Item
-          key={name}
-          name={name}
-          Icon={Icon}
-          highlight={isOnPath(pathname)}
-          url={url}
-          onClick={() => {
-            onClick?.(Router);
-          }}
-        />
-      ))}
-    </nav>
+    <div className="flex h-full w-full flex-col items-center justify-between">
+      <div className="w-full p-4 py-8">
+        <Link href="/dashboard" className="text-xl font-medium">
+          📝 AweNotes
+        </Link>
+      </div>
+      <nav className="flex w-full flex-col items-center justify-center gap-2 justify-self-center overflow-hidden">
+        {MENU_LINKS.map(({ name, url, icon: Icon, isOnPath }) => (
+          <Item
+            key={name}
+            name={name}
+            Icon={Icon}
+            highlight={isOnPath(pathname)}
+            url={url}
+          />
+        ))}
+      </nav>
+      <div className="flex w-full items-center justify-between p-4 text-zinc-400">
+        <span className="text-sm capitalize">{user?.name}</span>
+        <Tooltip text="Sair">
+          <button type="button" className="hover:text-zinc-300">
+            <LogOut size={24} />
+          </button>
+        </Tooltip>
+      </div>
+    </div>
   );
 }
 
@@ -56,47 +58,25 @@ function Item({
   Icon,
   highlight,
   url,
-  onClick,
 }: {
   name: string;
   Icon: LucideIcon;
   highlight: boolean;
-  url?: string;
-  onClick?: () => void;
+  url: string;
 }) {
-  const classes = clsx(
-    "flex w-11/12 items-center justify-center rounded-full py-2 text-zinc-500 transition-colors duration-200 hover:bg-zinc-700/40 hover:text-zinc-100",
-    {
-      "bg-zinc-700/40 text-zinc-300": highlight,
-    }
-  );
-
-  if (!url) {
-    return (
-      <button className={classes} onClick={onClick}>
-        <span className="flex w-3/4 items-center justify-center group-hover:justify-start">
-          <Icon size={27} className="shrink-0" />
-          <span className="ml-2 hidden whitespace-nowrap font-medium text-zinc-300 group-hover:block">
-            {name}
-          </span>
-        </span>
-      </button>
-    );
-  }
-
   return (
     <Link
       href={url}
       className={clsx(
-        "flex w-11/12 items-center justify-center rounded-full py-2 text-zinc-500 transition-colors duration-200 hover:bg-zinc-700/40 hover:text-zinc-100",
+        "flex w-11/12 items-center justify-center rounded-md py-2 text-zinc-500 transition-colors duration-200 hover:bg-zinc-700/40 hover:text-zinc-100",
         {
           "bg-zinc-700/40 text-zinc-300": highlight,
         }
       )}
     >
-      <span className="flex w-3/4 items-center justify-center group-hover:justify-start">
+      <span className="flex w-3/4 items-center justify-start">
         <Icon size={27} className="shrink-0" />
-        <span className="ml-2 hidden whitespace-nowrap font-medium text-zinc-300 group-hover:block">
+        <span className="ml-2 whitespace-nowrap font-medium text-zinc-300">
           {name}
         </span>
       </span>
